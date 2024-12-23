4e1e9e7fdd961ac87285509ec3e5309a25d8ade3
bahatilugusi <bahatilugusi2004@gmail.com>
2024-12-23 22:46:02 +0300

    creating Menu list in the display function

--- Banking_system.cpp
+++ Banking_system.cpp
@@ -17,4 +17,17 @@ void display()
   double balance = 0;
   double cash;
   int choice;
+
+   do
+  {
+    cout << "\n\t  Welcome To Bank Account Mnagement System";
+    cout << "\n\t******************************************";
+    cout << "\n1. Open New Accout";
+    cout << "\n2. Check Balance";
+    cout << "\n3. Deposite Cash.";
+    cout << "\n4. Withdraw Cash.";
+    cout << "\n5. Sim Banking.";
+    cout << "\n6. Exit.!";
+    cout << "\n\tPlease select your choice: ";
+    cin >> choice;
 }
