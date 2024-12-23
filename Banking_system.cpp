a549768bbd4f2c5f0bdbb6112f0a4788cc436930
bahatilugusi <bahatilugusi2004@gmail.com>
2024-12-23 22:51:58 +0300

    defining switch() option, to create Main menu functionality

--- Banking_system.cpp
+++ Banking_system.cpp
@@ -30,4 +30,40 @@ void display()
     cout << "\n6. Exit.!";
     cout << "\n\tPlease select your choice: ";
     cin >> choice;
+  
+  switch (choice)
+    {
+    case 1:
+      account();
+      break;
+    case 2:
+      cout << "\n\t\tYour initial balance is $" << balance;
+      break;
+
+    case 3:
+      cout << "\nEnter amount of Money to Depose: ";
+      cin >> cash;
+      balance += cash;
+      cout << "\n\t Your new Balance is $" << balance;
+      break;
+
+    case 4:
+      cout << "\nEnter amount of Money to Withdraw: ";
+      cin >> cash;
+      if (balance >= cash)
+      {
+        balance -= cash;
+        cout << "\n\tYour new balance is $" << balance;
+      }
+      else
+      {
+        cout << "\nYou do not have enough Balance";
+      }
+      break;
+
+    case 5:
+      simBanking();
+      break;
+    }
+  } while (choice != 6);
 }
